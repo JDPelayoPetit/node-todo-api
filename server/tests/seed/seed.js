d50ce3bf826ccf3ed0f5d2f1761820e05382fa5f
JD Pelayo Petit <pelayojose@gmail.com>
2017-04-02 23:33:36 -0400

    Asociate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -17,17 +17,23 @@ const users = [{
 }, {
   _id: userTwoId,
   email: 'pelayo@example.com',
-  password: 'userTwoPass'
+  password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+  }]
 }];
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   complete: true,
-  completeAt: 123
+  completeAt: 123,
+  _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
